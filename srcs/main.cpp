b7fb0c7ec99fcf7a8a0cf80629fbfaed43c17200
roduquen <duquenne.robin42@gmail.com>
2020-04-18 11:49:16 +0200

    Ajout du choix de l heuristique

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -10,12 +10,21 @@ void	algo_a_star(Goal *goal, OpenList &open_list, ClosedList &close_list)
 	}
 }
 
-int main(int args_count, char **args_value) {
-	if (args_count <= 1) {
-		std::cout << "You should write at least one argument" << std::endl;
+int		usage() {
+		std::cout << "N_Puzzle usage: n_puzzle [-0; -1; -2] [file]" << std::endl;
+		std::cout << "    -0: Manhattan Distance Heuristic" << std::endl;
+		std::cout << "    -1: Hamming Distance Heuristic" << std::endl;
+		std::cout << "    -2: Linear Conflict and Manhattan Distance Heuristic" << std::endl;
 		return (1);
+}
+
+int		main(int args_count, char **args_value) {
+	if (args_count != 3 || (strcmp(args_value[1], "-0") && strcmp(args_value[1], "-1") && strcmp(args_value[1], "-2"))) {
+		return (usage());
 	}
-	Lexer	*lexer = new Lexer(args_value[1]);
+
+	int		heuristic = args_value[1][1] - '0';
+	Lexer	*lexer = new Lexer(args_value[2]);
 	if (lexer->err == true) {
 		std::cout << "The file is not well formated : Lexer Error" << std::endl;
 		delete lexer;
@@ -36,16 +45,19 @@ int main(int args_count, char **args_value) {
 	}
 	else {
 		std::cout << "The puzzle is not solvable" << std::endl;
+		delete goal;
 		delete board_start;
 		return (1);
 	}
 
 	OpenList	open_list;
 	ClosedList	close_list;
-	
+
 	open_list.push(board_start);
 
 	algo_a_star(goal, open_list, close_list);
 
+	delete goal;
+	delete board_start;
 	return (0);
 }
