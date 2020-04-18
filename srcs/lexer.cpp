db1fa80b178958113ccb511f24ad1d9e073f6423
roduquen <duquenne.robin42@gmail.com>
2020-04-18 11:19:05 +0200

    fix open file

--- srcs/lexer.cpp
+++ srcs/lexer.cpp
@@ -2,89 +2,93 @@
 
 Lexer::Lexer(std::string path)
 {
-    std::ifstream file(path, std::ifstream::binary);
-    if (!file)
-    {
+	std::ifstream file(path, std::ifstream::binary);
+	if (!file)
+	{
 		std::cout << "File cannot be loaded" << std::endl;
 		err = true;
-    }
-    else
-    {
-        file.seekg(0, file.end);
-        length = file.tellg();
-        file.seekg(0, file.beg);
-        buffer = new char[length];
-        file.read(buffer, length);
-        if (!file)
-        {
-            err = true;
-        }
-        else
-        {
-            err = create_lexem();
-        }
-        file.close();
-    }
+	}
+	else
+	{
+		file.seekg(0, file.end);
+		length = file.tellg();
+		if (length <= 0) {
+			err = true;
+		}
+		else {
+			file.seekg(0, file.beg);
+			buffer = new char[length];
+			file.read(buffer, length);
+			if (!file) {
+				err = true;
+			}
+			else {
+				err = create_lexem();
+			}
+		}
+		file.close();
+
+	}
 }
 
 Lexer::~Lexer()
 {
-    delete lexem;
-    if (buffer)
-    {
-        delete buffer;
-    }
+	delete lexem;
+	if (buffer)
+	{
+		delete buffer;
+	}
 }
 
 bool Lexer::create_lexem()
 {
-    int state = START;
-    for (int i = 0; i < length; ++i)
-    {
-        if (state == HASHTAG)
-        {
-            if (buffer[i] == '\n')
-            {
-                lexem->push_back(ENDLINE);
-                state = ENDLINE;
-            }
-            else if (buffer[i] == '#')
-            {
-                lexem->push_back(SPACE);
-                state = SPACE;
-            }
-            else
-            {
-                lexem->push_back(HASHTAG);
-            }
-        }
-        else
-        {
-            if (buffer[i] == '#')
-            {
-                lexem->push_back(HASHTAG);
-                state = HASHTAG;
-            }
-            else if (isdigit(buffer[i]))
-            {
-                lexem->push_back(NUMBER);
-                state = NUMBER;
-            }
-            else if (buffer[i] == '\n')
-            {
-                lexem->push_back(ENDLINE);
-                state = ENDLINE;
-            }
-            else if (isspace(buffer[i]))
-            {
-                lexem->push_back(SPACE);
-                state = SPACE;
-            }
-            else
-            {
-                return (true);
-            }
-        }
-    }
-    return (false);
+	int state = START;
+	for (int i = 0; i < length; ++i)
+	{
+		if (state == HASHTAG)
+		{
+			if (buffer[i] == '\n')
+			{
+				lexem->push_back(ENDLINE);
+				state = ENDLINE;
+			}
+			else if (buffer[i] == '#')
+			{
+				lexem->push_back(SPACE);
+				state = SPACE;
+			}
+			else
+			{
+				lexem->push_back(HASHTAG);
+			}
+		}
+		else
+		{
+			if (buffer[i] == '#')
+			{
+				lexem->push_back(HASHTAG);
+				state = HASHTAG;
+			}
+			else if (isdigit(buffer[i]))
+			{
+				lexem->push_back(NUMBER);
+				state = NUMBER;
+			}
+			else if (buffer[i] == '\n')
+			{
+				lexem->push_back(ENDLINE);
+				state = ENDLINE;
+			}
+			else if (isspace(buffer[i]))
+			{
+				lexem->push_back(SPACE);
+				state = SPACE;
+			}
+			else
+			{
+				return (true);
+			}
+		}
+	}
+	return (false);
 }
